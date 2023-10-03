b8066b6fb14ecac9707d342cd2fa0ebdd16e7cd3
Brian Chan <brian.chan@liferay.com>
2023-10-03 12:15:53 +0100

    LPS-189573 Typo

--- modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-timebased-otp-web/src/main/java/com/liferay/multi/factor/authentication/timebased/otp/web/internal/checker/TimeBasedOTPBrowserSetupMFAChecker.java
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-timebased-otp-web/src/main/java/com/liferay/multi/factor/authentication/timebased/otp/web/internal/checker/TimeBasedOTPBrowserSetupMFAChecker.java
@@ -468,9 +468,9 @@ public class TimeBasedOTPBrowserSetupMFAChecker
 			return false;
 		}
 
-		String lastValidOTP = mfaTimeBasedOTPEntry.getLastValidTOTP();
+		String lastValidTOTP = mfaTimeBasedOTPEntry.getLastValidTOTP();
 
-		if (!mfaTimeBasedOTP.equals(lastValidOTP)) {
+		if (!mfaTimeBasedOTP.equals(lastValidTOTP)) {
 			return MFATimeBasedOTPUtil.verifyTimeBasedOTP(
 				_mfaTimeBasedOTPConfiguration.clockSkew(),
 				mfaTimeBasedOTPEntry.getSharedSecret(), mfaTimeBasedOTP);
