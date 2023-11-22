c18f5576c44f5fcaf8652480014304dd72f9ba90
Carolina Barbosa <carolina.barbosa@liferay.com>
2023-11-22 10:20:33 -0300

    LPS-202349 Add new upgrade process to delete the singleRecipient setting for User Notification types

--- modules/apps/notification/notification-service/src/main/java/com/liferay/notification/internal/upgrade/registry/NotificationUpgradeStepRegistrator.java
+++ modules/apps/notification/notification-service/src/main/java/com/liferay/notification/internal/upgrade/registry/NotificationUpgradeStepRegistrator.java
@@ -123,6 +123,11 @@ public class NotificationUpgradeStepRegistrator
 			"3.9.0", "3.9.1",
 			UpgradeProcessFactory.alterColumnType(
 				"NotificationTemplate", "type_", "VARCHAR(255) null"));
+
+		registry.register(
+			"3.9.1", "3.9.2",
+			new com.liferay.notification.internal.upgrade.v3_9_2.
+				NotificationRecipientSettingUpgradeProcess());
 	}
 
 	@Reference
