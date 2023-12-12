7c55bfab4f07cde1d6fcb65c078fe27a99b1d2c4
sergiojimcos <sergio.jdelcoso@liferay.com>
2023-12-12 14:29:52 +0100

    LPS-191342 Set a page and pageSize based on the limit configuration aand the parameters requested by the user

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/feature/VulcanFeature.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/feature/VulcanFeature.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.jaxrs.xml.JacksonXMLProvider;
 
 import com.liferay.depot.service.DepotEntryLocalService;
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.search.filter.Filter;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.ResourceActionLocalService;
@@ -123,7 +124,6 @@ public class VulcanFeature implements Feature {
 		featureContext.register(NotFoundExceptionMapper.class);
 		featureContext.register(ObjectMapperContextResolver.class);
 		featureContext.register(PageEntityExtensionWriterInterceptor.class);
-		featureContext.register(PaginationContextProvider.class);
 		featureContext.register(PrincipalExceptionMapper.class);
 		featureContext.register(RestrictFieldsQueryParamContextProvider.class);
 		featureContext.register(StatusDynamicFeature.class);
@@ -158,6 +158,9 @@ public class VulcanFeature implements Feature {
 
 		featureContext.register(
 			_nestedFieldsWriterInterceptor, Priorities.USER - 10);
+
+		featureContext.register(
+			new PaginationContextProvider(_configurationProvider, _portal));
 
 		featureContext.register(
 			new SiteParamConverterProvider(
@@ -197,6 +200,9 @@ public class VulcanFeature implements Feature {
 
 	@Reference
 	private ConfigurationAdmin _configurationAdmin;
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
 
 	@Reference
 	private DepotEntryLocalService _depotEntryLocalService;
