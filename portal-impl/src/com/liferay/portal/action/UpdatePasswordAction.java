MicaelleSilvaa/liferay-portal

commit e20f0071ca3f3db7fcb59988262b0e60901f618d

LPS-189689 use GetterUtil

portal-impl/src/com/liferay/portal/action/UpdatePasswordAction.java
@@ -160,7 +160,7 @@ public class UpdatePasswordAction implements Action {
 
 		try {
 			Ticket ticket = TicketLocalServiceUtil.fetchTicket(
-				Long.parseLong(ticketId));
+				GetterUtil.getLong(ticketId));
 
 			if ((ticket == null) ||
 				(ticket.getType() != TicketConstants.TYPE_PASSWORD)) {

commit c11063c87bdafcac96bbd92cfca625fbfb2a7492

LPS-189689 Fix compile error caused by e20f0071ca3f3db7fcb59988262b0e60901f618d

portal-impl/src/com/liferay/portal/action/UpdatePasswordAction.java
@@ -27,6 +27,7 @@ import com.liferay.portal.kernel.servlet.HttpMethods;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;