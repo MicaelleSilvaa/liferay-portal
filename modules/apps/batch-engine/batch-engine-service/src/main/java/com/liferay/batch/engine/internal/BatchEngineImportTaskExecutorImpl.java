076d45970026e785609b0e9c0524bc4200f69fa9
Carlos Correa <carlos.correa@liferay.com>
2024-02-07 12:43:12 +0100

    LPD-15236 Add companyId argument to scope every request by companyId.
    This is necessary because, given the following Object Definitions and companyIds:
        - C_Test & companyId 1
        - C_test & companyId 2
    
    If we call the method with itemClassName = com.liferay.object.rest.dto.v1_0.ObjectEntry and taskItemDelegateName = C_test from a request in the companyId = 1, the method will return an instance of BatchEngineTaskItemDelegate, but that should not happen, as the C_test taskItemDelegateName is only defined in companyId = 2.
    
    For that, in the following commits we need to scope by companyId all the BatchEngineTaskItemDelegates (if there is a companyId parameter, if not, it will be returned for all the companies)
    
    To get the BatchEngineTaskItemDelegate based on the company too, it must be scoped by the company, because it depends on the company that a taskItemDelegateName exists or not (an Object Definition given its name)
    
    ----

--- modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/BatchEngineImportTaskExecutorImpl.java
+++ modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/BatchEngineImportTaskExecutorImpl.java
@@ -68,6 +68,7 @@ public class BatchEngineImportTaskExecutorImpl
 	public void execute(BatchEngineImportTask batchEngineImportTask) {
 		BatchEngineTaskItemDelegate<?> batchEngineTaskItemDelegate =
 			_batchEngineTaskItemDelegateRegistry.getBatchEngineTaskItemDelegate(
+				batchEngineImportTask.getCompanyId(),
 				batchEngineImportTask.getClassName(),
 				batchEngineImportTask.getTaskItemDelegateName());
 
